d8f68daac611ed71e4aebff70f66c7e810969c97
Jose Chavarria <jose.chavarria@elaniin.com>
2019-03-14 14:39:14 -0600

    add foreings keys

--- database/migrations/2019_03_11_155626_create_user_details_table.php
+++ database/migrations/2019_03_11_155626_create_user_details_table.php
@@ -14,7 +14,7 @@ class CreateUserDetailsTable extends Migration
     public function up()
     {
         Schema::create('user_details', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('address');
