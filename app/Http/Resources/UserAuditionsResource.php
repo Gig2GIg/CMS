25cd0f865e095313076801202fa844744453e9fd
Jose Chavarria <jose.chavarria@elaniin.com>
2019-04-22 17:40:43 -0600

    add time to upcoming auditions

--- app/Http/Resources/UserAuditionsResource.php
+++ app/Http/Resources/UserAuditionsResource.php
@@ -9,27 +9,28 @@ class UserAuditionsResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
-        $dataProduction = explode(",",$this->auditions->production);
-        $url_media=$this->auditions->resources
-            ->where('type','image')
-            ->where('resource_type','App\Models\Auditions')
+        $dataProduction = explode(",", $this->auditions->production);
+        $url_media = $this->auditions->resources
+            ->where('type', 'image')
+            ->where('resource_type', 'App\Models\Auditions')
             ->pluck('url');
 
         return [
             'id' => $this->id,
-            'id_user'=> $this->auditions->user_id,
+            'id_user' => $this->auditions->user_id,
             'title' => $this->auditions->title,
-           'date' => $this->auditions->date,
+            'date' => $this->auditions->date,
+            'hour' => $this->auditions->time,
             'union' => $this->auditions->union,
-           'contract' => $this->auditions->contract,
+            'contract' => $this->auditions->contract,
             'production' => $dataProduction,
-            'media' =>$url_media[0] ?? null,
-            'number_roles'=>count($this->auditions->roles),
+            'media' => $url_media[0] ?? null,
+            'number_roles' => count($this->auditions->roles),
 
 
         ];
