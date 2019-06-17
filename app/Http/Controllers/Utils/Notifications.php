ec5931081616e4063189e5365ae1f5c9cd76059c
Jose Chavarria <jose.chavarria@elaniin.com>
2019-06-17 10:31:31 -0600

    adjust messge custom

--- app/Http/Controllers/Utils/Notifications.php
+++ app/Http/Controllers/Utils/Notifications.php
@@ -128,7 +128,7 @@ class Notifications
                                 'title' => $title,
                                 'code' => $type,
                                 'status' => 'unread',
-                                'message'=> $message
+                                'message'=> $message != null ? $message:$title
                             ]);
 
                             fcm()
