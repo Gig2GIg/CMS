949e2e3a97e8832f984ff942452199e949307e5d
Jose Chavarria <jose.chavarria@elaniin.com>
2019-06-20 09:03:31 -0600

    fix feedback user

--- app/Http/Controllers/FeedBackController.php
+++ app/Http/Controllers/FeedBackController.php
@@ -30,11 +30,11 @@ class FeedBackController extends Controller
     public function store(Request $request)
     {
         try {
-            if (Feedbacks::whereEvaluatorId($request->evaluator)->exists()) {
-                return response()->json([
-                    'data' => 'You have already done feedback.'
-                ], 409);
-            }
+//            if (Feedbacks::whereEvaluatorId($request->evaluator)->exists()) {
+//                return response()->json([
+//                    'data' => 'You have already done feedback.'
+//                ], 409);
+//            }
 
             $data = [
                 'auditions_id' => $request->auditions,
