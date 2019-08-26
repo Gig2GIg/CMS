3dfd827b6cb163c465f81f7b3f55e54b99bcac66
aponcax <alex.ponce@elaniin.com>
2019-08-26 15:53:26 -0600

    refactory filters to post by topics tags

--- app/Http/Resources/PostsTopicsWithPostResource.php
+++ app/Http/Resources/PostsTopicsWithPostResource.php
@@ -21,13 +21,13 @@ class PostsTopicsWithPostResource extends JsonResource
     public function toArray($request)
     {
         $postRepo = new PostsRepository(new Posts());
-        $post = $postRepo->find($this->topic_id);
+        $post = $postRepo->find($this->post_id);
 
         $userRepo = new UserRepository(new User());
         $user = $userRepo->find($post->user_id);
         $avatar = $user->image->url;
 
-        if ($post->type != 'director'){
+        if ($post->search_to != 'director'){
             return [
                 'id' => $post->id,
                 'title' => $post->title,
@@ -35,10 +35,10 @@ class PostsTopicsWithPostResource extends JsonResource
                 'type' => $post->type,
                 'url_media' => $post->url_media,
                 'avatar' => $avatar,
-                'url_media' => $this->url_media,
+                'url_media' => $post->url_media,
                 'name' => $user->details->first_name,
-                'time_ago' => $this->created_at->diffForHumans()
+                'time_ago' => $post->created_at->diffForHumans()
             ];
-        }   
+        }  
     }
 }
