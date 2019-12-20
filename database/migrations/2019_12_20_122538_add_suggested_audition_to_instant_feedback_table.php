fa7d0b6ccc99fc0c002edb1450f4767dc66257ec
Disha Mistry <disha.mistry@agileinfoways.com>
2019-12-20 20:40:14 +0530

    migration file added

---
+++ database/migrations/2019_12_20_122538_add_suggested_audition_to_instant_feedback_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSuggestedAuditionToInstantFeedbackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('instant_feedback', function (Blueprint $table) {
+                $table->integer('suggested_appointment_id')->unsigned();
+                $table->unique(['appointment_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('instant_feedback', function (Blueprint $table) {
+            //
+        });
+    }
+}
