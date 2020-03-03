cb2e9913c6697b7d8ff1237d513d8137fe79ae08
vijay.pampaniya <vijay.pampaniya@agileinfoways.com>
2020-03-03 15:24:55 +0530

    live user audition role_id migration issue fixed

--- database/migrations/2020_03_02_114210_chage_role_id_column_datatype_to_string_in_user_auditions_table.php
+++ database/migrations/2020_03_02_114210_chage_role_id_column_datatype_to_string_in_user_auditions_table.php
@@ -14,7 +14,8 @@ class ChageRoleIdColumnDatatypeToStringInUserAuditionsTable extends Migration
     public function up()
     {
         Schema::table('user_auditions', function (Blueprint $table) {
-            $table->string('rol_id', 255)->nullable()->change();
+            // $table->string('rol_id', 255)->nullable()->change();
+            DB::statement("ALTER TABLE `user_auditions` CHANGE `rol_id` `rol_id` VARCHAR( 255 ) NULL DEFAULT NULL");      
         });
     }
 
