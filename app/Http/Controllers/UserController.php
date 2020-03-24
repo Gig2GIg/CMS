cd427e0a0c5118f338e68fad78bb19f8b3fa8bdd
vijay.pampaniya <vijay.pampaniya@agileinfoways.com>
2020-03-24 11:21:15 +0530

    Performer profile name rename issue fixed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -284,7 +284,7 @@ class UserController extends Controller
             ];
             
             if($request->has('image') && $request->image != null){
-                $dataUser->image->update(['url' => $request->image, 'thumbnail' => $request->has('thumbnail') ? $request->thumbnail : NULL]);
+                $dataUser->image->update(['url' => $request->image, 'thumbnail' => $request->has('thumbnail') ? $request->thumbnail : NULL, 'name' => $request->has('file_name') ? $request->file_name : NULL]);
             }
             $userDetails = new UserDetailsRepository(new UserDetails());
             $dataUserDetails = $userDetails->findbyparam('user_id', $request->id);
