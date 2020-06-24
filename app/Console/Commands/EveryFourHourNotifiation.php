3a4be19f923508b754b7452362b74e1beabccbee
Vishal Raval <vishal.raval@agileinfoways.com>
2020-06-24 18:37:34 +0530

    check if upgraded plan found

--- app/Console/Commands/EveryFourHourNotifiation.php
+++ app/Console/Commands/EveryFourHourNotifiation.php
@@ -86,33 +86,35 @@ class EveryFourHourNotifiation extends Command
                     //Get next plan that suits the caster for their respective performer count in the talent DB
                     $upgradedPlan = $planRepo->where('allowed_performers', '>', $performerCount)->where('user_type', 1)->where('is_custom', 0)->where('is_active', 1)->get()->sortBy('allowed_performers')->first();
 
-                    $userData = $userRepo->find($subscription->user_id);
+                    if($upgradedPlan){
+                        $userData = $userRepo->find($subscription->user_id);
 
-                    $currentSubscription = $userData->subscriptions()->first();
-                    //swapping next upgraded plan for user
-                    $currentSubscription->noProrate()->swap($upgradedPlan->stripe_plan);
+                        $currentSubscription = $userData->subscriptions()->first();
+                        //swapping next upgraded plan for user
+                        $currentSubscription->noProrate()->swap($upgradedPlan->stripe_plan);
 
-                    $this->log->info("User subscription upgraded by CRON: From " . $currentSubscription->stripe_plan . " To ". $upgradedPlan->stripe_plan . " @" . Carbon::now('UTC')->format('Y-m-d H:i:s'));
+                        $this->log->info("User subscription upgraded by CRON: From " . $currentSubscription->stripe_plan . " To ". $upgradedPlan->stripe_plan . " @" . Carbon::now('UTC')->format('Y-m-d H:i:s'));
 
-                    //Update new subscription data to user subscription table
-                    $subscription->plan_id = $upgradedPlan->id;
-                    $subscription->name = $upgradedPlan->name;
+                        //Update new subscription data to user subscription table
+                        $subscription->plan_id = $upgradedPlan->id;
+                        $subscription->name = $upgradedPlan->name;
 
-                    $subscription->save();
+                        $subscription->save();
 
-                    //Getting next billing invoice
-                    $upcomingInvoice = $this->getUpcomingInvoice($userData);
+                        //Getting next billing invoice
+                        $upcomingInvoice = $this->getUpcomingInvoice($userData);
 
-                    //Sending mail to user about Upgradation
-                    $mail = new SendMail(); 
-                    $emailData = array();
-                    $emailData['name'] = $userData->details ? $userData->details->first_name . ' ' . $userData->details->last_name : '';
-                    $emailData['old_sub'] = $planRepo->find($currentSubscription->plan_id)->allowed_performers;
-                    $emailData['new_sub'] = $upgradedPlan->description;
-                    $emailData['new_amount'] = ($upcomingInvoice->amount_due / 100);
-                    $emailData['next_billing_date'] = Carbon::createFromTimeStamp($upcomingInvoice->next_payment_attempt)->format('Y-m-d H:i:s');
-                    
-                    $mail->sendUpgradeMail($userData->email, $emailData);
+                        //Sending mail to user about Upgradation
+                        $mail = new SendMail(); 
+                        $emailData = array();
+                        $emailData['name'] = $userData->details ? $userData->details->first_name . ' ' . $userData->details->last_name : '';
+                        $emailData['old_sub'] = $planRepo->find($currentSubscription->plan_id)->allowed_performers;
+                        $emailData['new_sub'] = $upgradedPlan->description;
+                        $emailData['new_amount'] = ($upcomingInvoice->amount_due / 100);
+                        $emailData['next_billing_date'] = Carbon::createFromTimeStamp($upcomingInvoice->next_payment_attempt)->format('Y-m-d H:i:s');
+                        
+                        $mail->sendUpgradeMail($userData->email, $emailData);
+                    }
                 }
             });                            
         } catch (\Exception $e) {
