2e38aa6e81d24cd337a895c21894fea490e1984f
Vishal Raval <vishal.raval@agileinfoways.com>
2020-07-14 19:07:53 +0530

    favorite and callback detected manually

--- app/Http/Controllers/FeedBackController.php
+++ app/Http/Controllers/FeedBackController.php
@@ -448,26 +448,42 @@ class FeedBackController extends Controller
                 ];
                 
                 AuditionLog::insert($roundData);
+
+                if((isset($oldData['favorite']) && $oldData['favorite'] != $newData['favorite']) || (isset($oldData['callback']) && $oldData['callback'] != $newData['callback'])){
+                    if(isset($oldData['favorite'])){
+                        AuditionLog::insert([
+                            'audition_id' => $appointment->auditions_id,
+                            'edited_by' => $this->getUserLogging(),
+                            'created_at' => Carbon::now('UTC')->format('Y-m-d H:i:s'),
+                            'key' => 'Feedback Starred',
+                            'old_value' => $oldData['favorite'] == 1 ? 'Yes' : 'No',
+                            'new_value' => $newData['favorite'] == 1 ? 'Yes' : 'No'
+                        ]);
+                    }
+
+                    if(isset($oldData['callback'])){
+                        AuditionLog::insert([
+                            'audition_id' => $appointment->auditions_id,
+                            'edited_by' => $this->getUserLogging(),
+                            'created_at' => Carbon::now('UTC')->format('Y-m-d H:i:s'),
+                            'key' => 'Feedback Callback',
+                            'old_value' => $oldData['callback'] == 1 ? 'Yes' : 'No',
+                            'new_value' => $newData['callback'] == 1 ? 'Yes' : 'No'
+                        ]);
+                    }
+                }
+
             }
             foreach ($multidimensional_diff_old as $key => $value) {
-                if($appointment && $appointment->auditions_id && $key != 'updated_at' && $key != 'slot_id' && $key != 'evaluator_id'){
+                if($appointment && $appointment->auditions_id && $key != 'updated_at' && $key != 'slot_id' && $key != 'evaluator_id' && $key == 'callback' && $key == 'favorite'){
                     $d = array();  
                     $d['audition_id'] = $appointment->auditions_id;
                     $d['edited_by'] = $this->getUserLogging();
                     $d['created_at'] = Carbon::now('UTC')->format('Y-m-d H:i:s');
-                    if($key == 'callback' || $key == 'favorite'){
-                        if($key == 'favorite'){
-                           $d['key'] = 'Feedback Starred';
-                        }else{
-                           $d['key'] = str_replace('_', ' ', 'Feedback ' . ucwords(strtolower($key))); 
-                        }
-                        $d['old_value'] = $value == 1 ? 'Yes' : 'No';
-                        $d['new_value'] = $multidimensional_diff_new[$key] == 1 ? 'Yes' : 'No';
-                    } else{
-                        $d['key'] = str_replace('_', ' ', 'Feedback ' . ucwords(strtolower($key)));
-                        $d['old_value'] = $value;
-                        $d['new_value'] = $multidimensional_diff_new[$key];
-                    }
+                    $d['key'] = str_replace('_', ' ', 'Feedback ' . ucwords(strtolower($key)));
+                    $d['old_value'] = $value;
+                    $d['new_value'] = $multidimensional_diff_new[$key];
+
                     array_push($insertData, $d);   
                 }
             }
