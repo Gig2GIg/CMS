db7e89568d941de3a481772e8af560a378eea9e6
Vishal Raval <vishal.raval@agileinfoways.com>
2020-07-24 17:47:20 +0530

    added plan and discounted column in table and checked in plan upgrade

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -8,7 +8,7 @@ class Plan extends Model
 {
     protected $table = 'plans';
 
-    protected $fillable = ['name', 'stripe_id', 'allowed_performers', 'description', 'amount', 'type', 'user_type'];
+    protected $fillable = ['name', 'stripe_plan', 'header', 'allowed_performers', 'description', 'amount', 'type', 'user_type', 'is_discounted', 'is_custom', 'is_active'];
 
     protected $dates = [
         'created_at', 'updated_at',
