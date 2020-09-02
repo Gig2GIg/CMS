7dbd4a75958d6885d3bbdc82ce2a01281dfc995c
Vishal Raval <vishal.raval@agileinfoways.com>
2020-09-02 20:39:33 +0530

    err fix

--- app/Http/Resources/AuditionFullResponse.php
+++ app/Http/Resources/AuditionFullResponse.php
@@ -51,7 +51,7 @@ class AuditionFullResponse extends JsonResource
         $appointment = $this->appointment()->where('status', 1)->first();
         if(!$appointment){
             $appointment = $this->appointment()->latest()->first();
-            $submissionsCount = OnlineMediaAudition::where('appointment_id', $appointment->id)->groupBy('performer_id')->count();
+            $submissionsCount = OnlineMediaAudition::where('appointment_id', $appointment->id)->groupBy('performer_id')->get()->count();
         }else{
             $submissionsCount = UserAuditions::where('type', 1)->where('appointment_id', $appointment->id)->count();
         }
