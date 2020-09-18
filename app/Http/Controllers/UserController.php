64dc47e0cca276c7c4015cbad2f39d3809a708ff
Vishal Raval <vishal.raval@agileinfoways.com>
2020-09-18 14:35:49 +0530

    fix admin function added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1680,7 +1680,7 @@ class UserController extends Controller
             $all = User::select('id', 'invited_by')->where('invited_by', '!=', null)->get();
             $insertData = array();
             $all->each(function ($v) use(&$insertData) {
-                array_push($insertData, ['admin_id' => $v->invited_by, 'member_id' => $v->id, 'created_at' => Carbon::now('UTC')->format('Y-m-d H:i:s')]);
+                array_push($insertData, ['admin_id' => $v->invited_by, 'member_id' => $v->id, 'is_selected' => 1, 'created_at' => Carbon::now('UTC')->format('Y-m-d H:i:s')]);
             });
             CasterTeam::insert($insertData);
             echo "Success";
